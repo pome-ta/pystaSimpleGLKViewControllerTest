465b4ec6663ef5407dd6ad2f4791f25aa10ccfd6
pome-ta <pome-ta@users.noreply.github.com>
2021-12-21 00:50:14 +0900

    up

--- uiModule.py
+++ uiModule.py
@@ -27,9 +27,9 @@ def glkView_drawInRect_(_self, _cmd, _view, rect):
   r, g, b = colorsys.hsv_to_rgb((time.time() * 0.1) % 1.0, 1, 1)
   glClearColor(r, g, b, 1.0)
   glClear(GL_COLOR_BUFFER_BIT)
-  view = ObjCInstance(_view)
-  print(f'view: {view.size().height}, {view.size().width}')
-  print(f'rect: {rect.size.height}, {rect.size.width}')
+  #view = ObjCInstance(_view)
+  #print(f'view: {view.size().height}, {view.size().width}')
+  #print(f'rect: {rect.size.height}, {rect.size.width}')
 
 
 MyGLViewDelegate = create_objc_class(
@@ -49,7 +49,7 @@ def dismiss(_self, _cmd):
 MyGLViewController = create_objc_class(
   'MyGLViewController', GLKViewController, methods=[dismiss])
 '''
-v = ui.View()#frame=(0,0,600,600))
+view = ui.View()#frame=(0,0,600,600))
 
 @on_main_thread
 def main():
@@ -65,10 +65,9 @@ def main():
   glvc.setTitle_('GLKit Demo')
   glvc.setView_(glview)
   
-  vo = ObjCInstance(v)    
-  v.present(style='fullscreen') #must be presented for nextResponder to work
-  vo.nextResponder().addChildViewController_(glvc)
-  vo.addSubview_(glview)
+  view.present(style='fullscreen') #must be presented for nextResponder to work
+  view.objc_instance.nextResponder().addChildViewController_(glvc)
+  view.objc_instance.addSubview_(glview)
   '''
   done_b = UIBarButtonItem.alloc().initWithTitle_style_target_action_('Done', 2, glvc, 'dismiss').autorelease()
   glvc.navigationItem().setRightBarButtonItem_(done_b)
